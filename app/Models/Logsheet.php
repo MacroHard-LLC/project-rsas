60161f5fe2ea576d0e4ed02d87c3366bc62580e6
Poop <70053528+AnimeAddiction@users.noreply.github.com>
2023-03-14 14:02:12 +0800

    Create Logsheet.php

---
+++ app/Models/Logsheet.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class Logsheet extends Authenticatable
+{
+    protected $table = 'logsheet';
+    protected $primaryKey = 'log_number';
+    protected $allowedFields = ['time', 'user_id', 'machine_id', 'room'];
+
+    // use HasApiTokens, HasFactory, Notifiable;
+
+    // /**
+    //  * The attributes that are mass assignable.
+    //  *
+    //  * @var array<int, string>
+    //  */
+    // protected $fillable = [
+    //     'name',
+    //     'email',
+    //     'password',
+    // ];
+
+    // /**
+    //  * The attributes that should be hidden for serialization.
+    //  *
+    //  * @var array<int, string>
+    //  */
+    // protected $hidden = [
+    //     'password',
+    //     'remember_token',
+    // ];
+
+    // /**
+    //  * The attributes that should be cast.
+    //  *
+    //  * @var array<string, string>
+    //  */
+    // protected $casts = [
+    //     'email_verified_at' => 'datetime',
+    // ];
+}
