e553737eccba82d73e6fbfc328603b5ef53b8edb
Poop <70053528+AnimeAddiction@users.noreply.github.com>
2023-03-14 17:38:22 +0800

    test

--- app/Models/Logsheet.php
+++ app/Models/Logsheet.php
@@ -2,13 +2,10 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Model;
 
-class Logsheet extends Authenticatable
+class Logsheet extends Model
 {
     protected $table = 'logsheet';
     protected $primaryKey = 'log_number';
