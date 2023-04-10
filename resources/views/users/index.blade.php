3ccf623efc12231a52c8fdc1cc65252d20e68e2e
thekatisdead <kaherbieto@up.edu.ph>
2023-04-10 09:18:24 +0800

    moved newsub to its own folder

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,6 +1,6 @@
 <x-layout>
     @include('users.create')
-    @include('newsub')
+    @include('subjects.newsub')
     <a class="btn btn-primary" role="button" data-bs-toggle="modal" data-bs-target="#registerModal">Create User</a>
     <a class="btn btn-primary" role="button" data-bs-toggle="modal" data-bs-target="#registerSubModal">Create Subject</a>
     @yield('scripts')
