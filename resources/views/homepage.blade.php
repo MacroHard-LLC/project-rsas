281c6c7c862380e5e659eaa39365536930f1243b
yzaolavides <mpolavides1@up.edu.ph>
2023-04-13 19:30:43 +0800

    fixed homepage frontend

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -7,28 +7,23 @@
 @include('users.create')
 @include('subjects.newsub')
 
-      <br><br><br>
-    
+<div class="container-fluid homepage">
+  <center>
+  <h1>WELCOME, USER!</h1>
+  <h4>What would you like to do today?</h4>
+  </center>
+</div>
+
       <div class="container pt-5">
       <fieldset>
         <div class="row my-3 gy-4">
-            
-            <h1 class = "homepage">WELCOME, USER!</h1>
-            <h4 class = "subtitle">What would you like to do today?</h2>
-
-            <br><br><br><br>
-{{--
-    <a class="btn btn-primary" role="button" data-bs-toggle="modal" data-bs-target="#registerModal">Create User</a>
-    <a class="btn btn-primary" role="button" data-bs-toggle="modal" data-bs-target="#registerSubModal">Create Subject</a>
-    --}}
-
-              <div class="container">
+              <div class="container options">
                 <div class="row justify-content-center">
                   <div class="col-sm-12">
                     <div class="d-grid gap-4 text-center">
                       <div class="row">
                         <div class="col-md-4">
-                          <button class="btn btn-primary create btn-create" type="button" onclick="location.href='http://www.example.com'">View User</button>
+                          <button class="btn btn-primary create btn-create" type="button" onclick="location.href='/users'">View User</button>
                         </div>
                         <div class="col-md-4">
                           <button class="btn btn-primary create btn-create" type="button" onclick="location.href='http://www.example.com'">View Section</button>
@@ -39,33 +34,23 @@
                       </div>
                       <div class="row">
                         <div class="col-md-4">
-                          <a data-bs-toggle="modal" data-bs-target="#registerModal">
-                          <button class="btn btn-primary create btn-create" type="button">Create User</button>
-                          </a>
+                          <button class="btn btn-primary create btn-create" type="button" onclick="location.href='http://www.example.com'">Create User</button>
+                          {{--<a class="btn btn-primary" role="button" data-bs-toggle="modal" data-bs-target="#registerModal">Create User</a>--}}
                         </div>
                         <div class="col-md-4">
-                          
                           <button class="btn btn-primary create btn-create" type="button" onclick="location.href='http://www.example.com'">Create Section</button>
-                        
                         </div>
                         <div class="col-md-4">
-                          <a data-bs-toggle="modal" data-bs-target="#registerSubModal">
-                          <button class="btn btn-primary create btn-create" type="button">Create Subject</button>
-                          </a>
+                          <button class="btn btn-primary create btn-create" type="button" onclick="location.href='http://www.example.com'">Create Subject</button>
                         </div>
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
-            
-        
+          
         </div>
-
-
       </fieldset>
     </div>
-    
 
-</body>
-</html> 
+@endsection
