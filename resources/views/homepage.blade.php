e7fad0b64938e593a9694c742a117699b9faf8a4
Shyggi <sdobado@up.edu.ph>
2023-04-13 16:49:14 +0800

    done(?) converting homepage layout to views

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,2 +1,58 @@
+<link rel="stylesheet" href="{{ asset('css/homepage.css') }}">
+
 @extends('master')
 @section('content')
+
+      <br><br><br>
+    
+      <div class="container pt-5">
+      <fieldset>
+        <div class="row my-3 gy-4">
+            
+            <h1 class = "homepage">WELCOME, USER!</h1>
+            <h4 class = "subtitle">What would you like to do today?</h2>
+
+            <br><br><br><br>
+            
+
+              <div class="container">
+                <div class="row justify-content-center">
+                  <div class="col-sm-12">
+                    <div class="d-grid gap-4 text-center">
+                      <div class="row">
+                        <div class="col-md-4">
+                          <button class="btn btn-primary create btn-create" type="button" onclick="location.href='http://www.example.com'">View User</button>
+                        </div>
+                        <div class="col-md-4">
+                          <button class="btn btn-primary create btn-create" type="button" onclick="location.href='http://www.example.com'">View Section</button>
+                        </div>
+                        <div class="col-md-4">
+                          <button class="btn btn-primary create btn-create" type="button" onclick="location.href='http://www.example.com'">View Subject</button>
+                        </div>
+                      </div>
+                      <div class="row">
+                        <div class="col-md-4">
+                          <button class="btn btn-primary create btn-create" type="button" onclick="location.href='http://www.example.com'">Create User</button>
+                        </div>
+                        <div class="col-md-4">
+                          <button class="btn btn-primary create btn-create" type="button" onclick="location.href='http://www.example.com'">Create Section</button>
+                        </div>
+                        <div class="col-md-4">
+                          <button class="btn btn-primary create btn-create" type="button" onclick="location.href='http://www.example.com'">Create Subject</button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            
+        
+        </div>
+
+
+      </fieldset>
+    </div>
+    
+
+</body>
+</html> 
