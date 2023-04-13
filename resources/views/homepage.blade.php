2e02ed55fb1143a0176beaa12756ee848e9a38c6
Shyggi <sdobado@up.edu.ph>
2023-04-13 20:32:49 +0800

    changed buttons appearance

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -24,28 +24,28 @@
             <div class="container options">
                 <div class="row justify-content-center">
                 <div class="col-sm-12">
-                    <div class="d-grid gap-4 text-center">
+                    <div class="d-grid gap-5 text-center">
                     <div class="row">
-                        <div class="col-md-4">
+                        <div class="col-md-4" style="width:300px">
                         <button class="btn btn-primary create btn-create" type="button" onclick="location.href='/users'">View User</button>
                         </div>
-                        <div class="col-md-4">
+                        <div class="col-md-4" style="width:300px">
                         <button class="btn btn-primary create btn-create" type="button" onclick="location.href='http://www.example.com'">View Section</button>
                         </div>
-                        <div class="col-md-4">
+                        <div class="col-md-4" style="width:300px">
                         <button class="btn btn-primary create btn-create" type="button" onclick="location.href='http://www.example.com'">View Subject</button>
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col-md-4">
+                        <div class="col-md-4" style="width:300px">
                         <button class="btn btn-primary create btn-create" type="button">
                             <a data-bs-toggle="modal" data-bs-target="#registerModal">Create User</a></button>
                         {{--<a class="btn btn-primary" role="button" data-bs-toggle="modal" data-bs-target="#registerModal">Create User</a>--}}
                         </div>
-                        <div class="col-md-4">
+                        <div class="col-md-4" style="width:300px">
                         <button class="btn btn-primary create btn-create" type="button" onclick="location.href='http://www.example.com'">Create Section</button>
                         </div>
-                        <div class="col-md-4">
+                        <div class="col-md-4" style="width:300px">
                         <button class="btn btn-primary create btn-create" type="button">
                             <a data-bs-toggle="modal" data-bs-target="#registerSubModal">Create Subject</a></button>
                         </div>
