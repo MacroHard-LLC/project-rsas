805b4cca1e9c988c9e3c2c316045f60dc52c16e7
thekatisdead <kaherbieto@up.edu.ph>
2023-04-18 00:04:47 +0800

    unit testing for Create + View Subject Complete

--- app/Models/Subject_table.php
+++ app/Models/Subject_table.php
@@ -9,7 +9,11 @@ class Subject_table extends Model
 {
     //Needs more than one primary key
     public $table = 'subject';
-    public $primaryKey = 'grade_level';
+    public $primaryKey = 'id';
+
+    // this is the thing for the unit testing
+    protected $fillable = ['id', 'name', 'grade_level','instructor_id','room','year_start',
+                            'year_end','added_on','added_by','is_deleted'];
 
     // timestamps is FALSE as to defer the Laravel's need for
     // UPDATED_AT where we already have updated_on in our database
