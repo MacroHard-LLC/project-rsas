9e84e7b2d511e33153e832d1b59ccf5cfc0617bf
thekatisdead <kaherbieto@up.edu.ph>
2023-05-01 14:18:36 +0800

    routed AdviserViews

---
+++ app/Http/Controllers/AdviserViewController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AdviserViewController extends Controller
+{
+    function AdviserPage(){
+        return view('adviser.adviserpage');
+    }
+}
