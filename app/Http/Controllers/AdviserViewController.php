ac1540702c06f51357694e5ad2fca60f2a7bea36
thekatisdead <kaherbieto@up.edu.ph>
2023-05-04 13:59:37 +0800

    can add students based on the adviser id

--- app/Http/Controllers/AdviserViewController.php
+++ app/Http/Controllers/AdviserViewController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use App\Models\Section;
 use App\Models\Student;
+use App\Models\PresentAttendance;
+use App\Models\LateAttendance;
 use Illuminate\Http\Request;
 
 class AdviserViewController extends Controller
@@ -37,8 +39,34 @@ class AdviserViewController extends Controller
     function GetAllStudents(){
         $incoming_data = session()->get('section_id');
         $query = Student::where('section_id', $incoming_data)
-                 ->get();
-        return $query;
+                 ->get('user_id');
+        $attendanceArray = array();
+        foreach($query as $student){
+            $first_name = User::where('id',$student['user_id'])->value('first_name');
+            $last_name = User::where('id',$student['user_id'])->value('last_name');
+            $name = $last_name . ', ' . $first_name;
+
+            $is_present = PresentAttendance::where('student_id',$student['user_id'])
+                        ->exists();
+            $is_late = LateAttendance::where('student_id',$student['user_id'])
+                        ->exists();
+            $status = 'Absent';
+
+            if($is_present){
+                $status = 'Present';
+            }
+            else if($is_late){
+                $stauts = 'Late';
+            }
+            $attendanceArray[] = array(
+                'id' => $student['user_id'],
+                'name' => $name,
+                'status' => $status,
+            );
+        }
+        
+        
+        return $attendanceArray;
     }
 
     function StudentPage(){
