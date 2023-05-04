655e4bbf2945bfa21c5a064f243e04229739cc3b
thekatisdead <kaherbieto@up.edu.ph>
2023-05-04 10:51:32 +0800

    added a few more functions to the controller

--- app/Http/Controllers/AdviserViewController.php
+++ app/Http/Controllers/AdviserViewController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Models\Section;
 use Illuminate\Http\Request;
 
 class AdviserViewController extends Controller
@@ -19,12 +20,21 @@ class AdviserViewController extends Controller
     }
 
     function start(Request $request){
+        // this is the adviser id request.
         $incoming_id = $request->input_data;
+
+        // section id
+        $sectionId = Section::where('adviser_id','=',$incoming_id)->value('id');
+        session()->put('section_id',$sectionId);
         $first = User::where('id','=',$incoming_id)->value('first_name');
         $middle = User::where('id','=',$incoming_id)->value('middle_name');
         $last = User::where('id','=',$incoming_id)->value('last_name');
         $nameID = $last . ", " . $first . " " . $middle;
         return $nameID;
+    }
+
+    function GetAllStudents(){
+        
     }
 
     function StudentPage(){
