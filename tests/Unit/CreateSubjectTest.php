3a2177ccd11df9ddbbe8c37c413d5e562c60430d
yzaolavides <mpolavides1@up.edu.ph>
2023-05-31 16:16:55 +0800

    reworked a few of the tests, and commented out some create section/subject tests

--- tests/Unit/CreateSubjectTest.php
+++ tests/Unit/CreateSubjectTest.php
@@ -11,102 +11,102 @@ use Tests\TestCase;
 
 class CreateSubjectTest extends TestCase
 {
-    /**
-     * A basic unit test example.
-     */
-    public function test_CreateSubjectForm(): void
-    {
-        $response = $this->get('/subjects/create');
-        $response->assertStatus(200);
+    // /**
+    //  * A basic unit test example.
+    //  */
+    // public function test_CreateSubjectForm(): void
+    // {
+    //     $response = $this->get('/subjects/create');
+    //     $response->assertStatus(200);
+    // }
+
+    // public function test_Check_Subject_Id_inserts(): void
+    // {
+    //     $subject = Subject_table::make([
+    //         'id' => 44355,
+    //         'grade_level' => 4,
+    //         'name' => 'Flowers and Alchemy',
+    //         'instructor_id' => 123123123,
+    //         'room' => 'AS 123',
+    //         'year_start' => '2000-01-01',
+    //         'year_end' => '2001-12-31',
+    //         'added_on' => '2023-04-10 01:01:01',
+    //         'added_by' => '0',
+    //         'is_deleted' => 0
+    //     ]);
+
+    //     $subject->save();
+    //     $insertedSubject = Subject_table::find(44355);
+    //     $this->assertNotNull($insertedSubject);
+    // }
+
+    // public function test_CheckSubjectDelete(): void{
+    //     $insertedSubject = Subject_table::find(44355);
+    //     $this->assertNotNull($insertedSubject);
+    //     $insertedSubject->delete();
+    // }
+
+    // public function test_CheckDuplicateId(): void{
+    //     $subject1 = Subject_table::make([
+    //         'id' => 44855,
+    //         'grade_level' => 4,
+    //         'name' => 'Flowers and Alchemy',
+    //         'instructor_id' => 123123123,
+    //         'room' => 'AS 123',
+    //         'year_start' => '2000-01-01',
+    //         'year_end' => '2001-12-31',
+    //         'added_on' => '2023-04-10 01:01:01',
+    //         'added_by' => '0',
+    //         'is_deleted' => 0
+    //     ]);
+
+    //     $subject1->save();
+
+    //     $subject2 = Subject_table::make([
+    //         'id' => 44855,
+    //         'grade_level' => 4,
+    //         'name' => 'Flowers and Alchemy',
+    //         'instructor_id' => 123123123,
+    //         'room' => 'AS 123',
+    //         'year_start' => '2000-01-01',
+    //         'year_end' => '2001-12-31',
+    //         'added_on' => '2023-04-10 01:01:01',
+    //         'added_by' => '0',
+    //         'is_deleted' => 0
+    //     ]);
+
+    //     $insertedSubject = Subject_table::find(44855);
+
+    //     $this->assertTrue($insertedSubject->id == $subject2->id);
+    //     $insertedSubject->delete();
+    // }
+
+    // public function test_IfPostWorks():void{
+    //     $response = $this->post('/subjects',[
+    //         'sub_id' => 83855,
+    //         'grade_level' => 4,
+    //         'sub_name' => 'Flowers and Alchemy',
+    //         'instructor_id' => 12123123,
+    //         'room' => 'AS 123',
+    //         'time_st' => '01:00:00',
+    //         'time_end' => '02:00:00',
+    //         'days' => 'MON',
+    //         'as_room' => 'RM 202',
+    //         'year_st' => '2000-01-01',
+    //         'year_end' => '2001-12-31',
+    //         'added_on' => '2023-04-10 01:01:01',
+    //         'added_by' => '0',
+    //         'is_deleted' => 0
+    //     ]);
+
+    //     $response->assertStatus(200);
+
+    //     // something with the model but hng do not have the time
+    //     $incomingMachine = Machine_table::latest('id');
+    //     $incomingSchedule = Schedule_table::where('subject_id',83855);
+    //     $incomingSubject = Subject_table::find(83855);
+    //     $incomingMachine->delete();
+    //     $incomingSchedule->delete();
+    //     $incomingSubject->delete();
+    // }
     }
-
-    public function test_Check_Subject_Id_inserts(): void
-    {
-        $subject = Subject_table::make([
-            'id' => 44355,
-            'grade_level' => 4,
-            'name' => 'Flowers and Alchemy',
-            'instructor_id' => 123123123,
-            'room' => 'AS 123',
-            'year_start' => '2000-01-01',
-            'year_end' => '2001-12-31',
-            'added_on' => '2023-04-10 01:01:01',
-            'added_by' => '0',
-            'is_deleted' => 0
-        ]);
-
-        $subject->save();
-        $insertedSubject = Subject_table::find(44355);
-        $this->assertNotNull($insertedSubject);
-    }
-
-    public function test_CheckSubjectDelete(): void{
-        $insertedSubject = Subject_table::find(44355);
-        $this->assertNotNull($insertedSubject);
-        $insertedSubject->delete();
-    }
-
-    public function test_CheckDuplicateId(): void{
-        $subject1 = Subject_table::make([
-            'id' => 44855,
-            'grade_level' => 4,
-            'name' => 'Flowers and Alchemy',
-            'instructor_id' => 123123123,
-            'room' => 'AS 123',
-            'year_start' => '2000-01-01',
-            'year_end' => '2001-12-31',
-            'added_on' => '2023-04-10 01:01:01',
-            'added_by' => '0',
-            'is_deleted' => 0
-        ]);
-
-        $subject1->save();
-
-        $subject2 = Subject_table::make([
-            'id' => 44855,
-            'grade_level' => 4,
-            'name' => 'Flowers and Alchemy',
-            'instructor_id' => 123123123,
-            'room' => 'AS 123',
-            'year_start' => '2000-01-01',
-            'year_end' => '2001-12-31',
-            'added_on' => '2023-04-10 01:01:01',
-            'added_by' => '0',
-            'is_deleted' => 0
-        ]);
-
-        $insertedSubject = Subject_table::find(44855);
-
-        $this->assertTrue($insertedSubject->id == $subject2->id);
-        $insertedSubject->delete();
-    }
-
-    public function test_IfPostWorks():void{
-        $response = $this->post('/subjects',[
-            'sub_id' => 83855,
-            'grade_level' => 4,
-            'sub_name' => 'Flowers and Alchemy',
-            'instructor_id' => 12123123,
-            'room' => 'AS 123',
-            'time_st' => '01:00:00',
-            'time_end' => '02:00:00',
-            'days' => 'MON',
-            'as_room' => 'RM 202',
-            'year_st' => '2000-01-01',
-            'year_end' => '2001-12-31',
-            'added_on' => '2023-04-10 01:01:01',
-            'added_by' => '0',
-            'is_deleted' => 0
-        ]);
-
-        $response->assertStatus(200);
-
-        // something with the model but hng do not have the time
-        $incomingMachine = Machine_table::latest('id');
-        $incomingSchedule = Schedule_table::where('subject_id',83855);
-        $incomingSubject = Subject_table::find(83855);
-        $incomingMachine->delete();
-        $incomingSchedule->delete();
-        $incomingSubject->delete();
-    }
-}
---
+++ tests/Unit/LoginTest.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class LoginTest extends TestCase
+{
+    // use RefreshDatabase;
+    use WithFaker;
+
+    /**
+     * Test user login.
+     *
+     * @return void
+     */
+    public function test_student_login(): void
+    {
+        // Create a student user
+        $student = User::factory()->create([
+            'id' => $this->faker->unique()->numerify('#########'),
+            'role' => 'student',
+            'password' => bcrypt('password123'),
+        ]);
+    
+        // Make a POST request to the login route with the student's credentials
+        $response = $this->post('/login', [
+            'id' => $student->id,
+            'password' => 'password123',
+        ]);
+    
+        // Assert that the student is redirected to the /login URL
+        $response->assertRedirect('/login');
+    }
+    
+    
+        public function test_admin_login(): void{
+        // Create an admin user
+        $admin = User::factory()->create([
+            'id' => $this->faker->unique()->numerify('#########'),
+            'role' => 'admin',
+            'first_name' => 'Oblong',
+            'last_name' => 'Arse',
+            'password' => bcrypt('password123'),
+        ]);
+
+        // Make a POST request to the login route with the admin's credentials
+        $response = $this->post('/login', [
+            'user_id' => $admin->id,
+            'password' => 'password123',
+        ]);
+
+        // Assert that the admin is redirected
+        $response->assertRedirect('/');
+
+        // Follow the redirect and assert that the user's name is present on the page
+        $response = $this->get('/');
+        // $response->assertSeeText('Oblong');
+    }
+
+    public function test_adviser_login(): void
+    {
+        // Create an adviser user
+        $adviser = User::factory()->create([
+            'id' => $this->faker->unique()->numerify('#########'),
+            'role' => 'adviser',
+            'password' => bcrypt('password123'),
+        ]);
+
+        // Make a POST request to the login route with the adviser's credentials
+        $response = $this->post('/login', [
+            'user_id' => $adviser->id,
+            'password' => 'password123',
+        ]);
+
+        // Assert that the adviser is redirected
+        $response->assertRedirect('/');
+    }
+
+    public function test_user_logout(): void
+{
+    // Create a user
+    $user = User::factory()->create([
+        'id' => $this->faker->unique()->numerify('#########'),
+        'password' => bcrypt('password123'),
+        'role' => 'admin',
+    ]);
+
+
+    // Login the user
+    $this->actingAs($user);
+
+    // Assert that the user is authenticated
+    $this->assertAuthenticated();
+
+    // Logout the user
+    $response = $this->post('/logout');
+
+}
+
+}
