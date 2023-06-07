3322f137c563355988cf3fa8be088b3df2775578
thekatisdead <kaherbieto@up.edu.ph>
2023-06-07 20:12:17 +0800

    api moved to api.php

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+// RFID Controller
+Route::post('/rfid/tap', 'RFIDController@tap');
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
