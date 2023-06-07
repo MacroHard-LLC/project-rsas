87708e0cb6a97544e606c769b2a5b9c4f1bb24c4
Pris <ptsalvador@up.edu.ph>
2023-06-07 11:37:53 +0800

    incomplete

--- app/Http/Controllers/AdviserViewController.php
+++ app/Http/Controllers/AdviserViewController.php
@@ -155,4 +155,43 @@ class AdviserViewController extends Controller
         return view('adviser.editattendance');
     }
 
+    function StudentInfo($id){
+        $incoming_data = session()->get('adviser_id');
+            $sectionId = Section::where('adviser_id','=',$incoming_data)->first()->id;
+    
+            $query = Student::where('section_id', $sectionId)
+                     ->get('user_id');
+            $attendanceArray = array();
+            foreach($query as $student){
+                $first_name = User::where('id',$student['user_id'])->value('first_name');
+                $last_name = User::where('id',$student['user_id'])->value('last_name');
+                $name = $last_name . ', ' . $first_name;
+    
+                $is_dropped = Dropped::where('student_id',$student['user_id'])
+                            ->where('status', 1)->exists();
+                $is_not_dropped = Dropped::where('student_id',$student['user_id'])
+                            ->where('status', 0)->exists();
+    
+                $student_tag = session() ->get('new_status');
+    
+                if($student['user_id'] == $student_tag){
+                    $status = session()->get('new_status');
+                }
+                else if($is_dropped){
+                    $status = 'Dropped';
+                }
+                else if($is_not_dropped){
+                    $status = 'Enrolled';
+                }
+                $attendanceArray[] = array(
+                    'name' => $name,
+                    'status' => $status,
+                );
+            }
+            
+            
+            return $attendanceArray;
+    
+        }
+
 }
--- resources/views/adviser/studentinfo.blade.php
+++ resources/views/adviser/studentinfo.blade.php
@@ -5,8 +5,33 @@
 
 @extends('master')
 @section('content')
+@section('scripts')
+@include('adviser.editattendance')
 
 @include('adviser.tag')
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script>
+
+    $(document).ready(function() {
+        var adviserID = {{ $adviserId }};
+        $.ajax({
+            method: "POST",
+            headers: {
+                    'X-CSRF-TOKEN': '{{ csrf_token() }}',
+                    Accept: "application/json"
+            },
+            url: "{{ route('adviser_startup') }}",
+            data: { input_data: adviserID },
+            success: function(data) {
+                console.log(data);
+                $('#attendanceAdviserName').text(data);
+            }
+        });
+        const tableBody = document.querySelector('#attendanceTable tbody');
+        let subject = $('#subject_adviserView_dropdown');
+        console.log(subject);
+    });
 
 <div class="container mx-10 my-5">
 <div class="border rounded px-4 pt-5 pb-3 my-2">
@@ -36,6 +61,18 @@
         <div class="row">
             <div class="col">
                 <table class="table table-hover" id="attendanceTable" data-toggle="table" data-toolbar="#toolbar">
+                <thead>
+                        <tr>
+                            <th class="w-75">Student Name</th>
+                            <th data-align="right">Status</th>
+                            <th data-align="left">Tag</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <!-- Table rows will be dynamically added here -->
+                    </tbody>
+
                     <tr>
                         <th class="w-10"></th>
                         <th class="w-75">Student Name</th>
