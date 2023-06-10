d3ebab98e8da7d6495a5868c942e0db769b8e717
kuanhh <hannahbenitez62@gmail.com>
2023-06-10 21:31:38 +0800

    add name fields + validation to Create Instructor modal

--- public/js/createInstructor.js
+++ public/js/createInstructor.js
@@ -19,7 +19,6 @@ window.addEventListener('load', function() {
                 var form_control = form_children.find('.form-control')
                 var valid_form_control = form_children.find('.form-control.is-valid')
                 var is_valid = form_control.length === valid_form_control.length;
-
                 if (is_valid)
                     document.getElementById("submit_instructor").style.visibility = "visible";
                 else
@@ -30,8 +29,14 @@ window.addEventListener('load', function() {
 });
 
 function showAddInstructorError(input){
-    if (input.validity.patternMismatch)
-        $("#" + input.getAttribute("name") + "_error").children("span").text("RFID number must be an integer.");
-    else if (input.validity.tooShort)
-        $("#" + input.getAttribute("name") + "_error").children("span").text("RFID number must be 10 digits.");
+    var input_name = input.getAttribute("name");
+    if (input_name == "instructor_rfid_number"){
+        if (input.validity.patternMismatch)
+            $("#" + input.getAttribute("name") + "_error").children("span").text("RFID number must be an integer.");
+        else if (input.validity.tooShort)
+            $("#" + input.getAttribute("name") + "_error").children("span").text("RFID number must be 10 digits.");
+    } else if (input_name == "instructor_first_name" || input_name == "instructor_middle_name" || input_name == "instructor_last_name"){
+        if (input.validity.patternMismatch)
+            $("#" + input.getAttribute("name") + "_error").children("span").text("Name must only be alphabetic characters.");
+    }
 }
